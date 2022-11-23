fe52610c31d048e90fdbbdb284a91b348fa7d79e
unknown <muh.it.eng.19841984@gmail.com>
2022-11-23 08:39:27 +0200

    Simple login

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
-const PORT = 8000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {});
+// middleware to parse request
+app.use(express.json());
 
+//Routes
+app.get("/", (req, res) => {
+  res.status(200).send("Server working ....");
+});
+
+app.post("/login", (req, res) => {
+  const userName = req.body.username;
+  const password = req.body.password;
+
+  if (userName === "Moh" && password === "123") {
+    res.status(200).json({ status: true, message: "login successfully done" });
+  }
+
+  res.status(401).json({ status: true, message: "login failed" });
+});
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
